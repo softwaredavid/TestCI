e0eb004f2f52e4c9ee3d3f0e55ae1c6fe65b36d9
dst <softwaredavid@sina.com>
2020-04-24 12:31:53 +0800

    test CI

---
+++ TestCI/TestCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCI
+//
+//  Created by mac on 2020/4/24.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
